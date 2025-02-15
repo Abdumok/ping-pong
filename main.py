6407f6f1383bd699bf9a474151e22320024e9fe6
Abdullahi Mohammmed Ali <abdumok19@gmail.com>
2025-02-15 20:34:10 +0300

    initial commit and setup main screen

---
+++ main.py
@@ -0,0 +1,14 @@
+from turtle import Screen
+
+#Setup Main Screen
+window = Screen()
+window.bgcolor('black')
+window.setup(width=800, height=600)
+window.title('Ping Pong v1.00')
+
+
+
+
+
+
+window.exitonclick()
