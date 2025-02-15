8a1fd138036e49bdee9c8b1d4d31ff5a3aa39268
Abdullahi Mohammmed Ali <abdumok19@gmail.com>
2025-02-15 20:45:39 +0300

    initial paddel module and create l_paddle and r_paddle objects

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from turtle import Screen
+from paddle import Paddle
 
 #Setup Main Screen
 window = Screen()
@@ -7,6 +8,9 @@ window.setup(width=800, height=600)
 window.title('Ping Pong v1.00')
 
 
+# Create Paddle:
+r_paddle= Paddle(position=(370, 0))
+l_paddle= Paddle(position=(-380, 0))
 
 
 
